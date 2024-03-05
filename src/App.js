a5943303424fac2505221307431b00556ea2c14b
Varun Kumar <varunkumar63931@gmail.com>
2024-03-05 23:56:32 +0530

    dnbdhh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+
+
+let Name="Varun"
 
 function App() {
+
+  const[comment,setComment]=useState("");
+  const[comments,setComments]=useState([]);
+
+  const onclickHandler= () => {
+    setComment((comments) => [...comments ,comment]);
+  };
+
+
+  const onChangeHandler =(e) => {
+    setComment(e.target.value);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with me
-        </a>
-      </header>
-    </div>
+<>
+   
+<Navbar title="Navbar" aboutText="About us"/>
+<div className='main-container'>
+  {comment.map((text)=>(
+
+<div className='comment-container py-3'>{text}</div>
+  ))}
+    
+
+ <div className='comment-flexbox'>
+  <h3 className='comment-text'>COMMENT</h3>
+  <textarea value={comment} 
+  onChange={onChangeHandler} 
+  className='input-box'/>
+  <button onClick={onChangeHandler} className='btn btn-primary'>Submit</button>
+</div>
+</div>
+</>
+    
   );
 }
 
